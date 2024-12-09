46505eafb63ab8e3a4a9376da2f6ac3c05ccdf06
Theo1016 <Theo1016@users.noreply.github.com>
2024-12-09 18:34:33 +0800

    Add files via upload

--- contracts/CIFDTokenToAnotherChain.sol
+++ contracts/CIFDTokenToAnotherChain.sol
@@ -8,20 +8,8 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/utils/structs/EnumerableMap.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
-contract CIFDToken is ERC20,CCIPReceiver,Ownable{
+contract CIFDTokenToAnotherChain is ERC20,CCIPReceiver,Ownable{
     using EnumerableMap for EnumerableMap.Bytes32ToUintMap;
-    address public foundersWallet;
-    address public investorsWallet;
-    address public ecosystemWallet;
-
-    uint256 public foundersTokens;
-
-    uint256 public unlockTime1Year;
-    uint256 public unlockTime2Years;
-    uint256 public unlockTime3Years;
-    uint256 public unlockTime4Years;
-    uint256 public initFounder;
-    uint256 public maxSupply;
     bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
     address private s_lastReceivedTokenAddress; // Store the last received token address.
     uint256 private s_lastReceivedTokenAmount; // Store the last received amount.
@@ -60,69 +48,25 @@ contract CIFDToken is ERC20,CCIPReceiver,Ownable{
     );
     
 
+     // CCIP相关变量
      IRouterClient private router;
-     address private linkTokenAddress; 
+     address private linkTokenAddress; // LINK代币地址
  
+     // CCIP事件
      event CcipSend(address indexed sender, address indexed receiver, uint256 amount, bytes data, uint64 destinationChainId);
 
     
-     constructor(address _foundersWallet, address _investorsWallet, address _ecosystemWallet, address _router, address _link)
-     ERC20("CIFD Shares", "CIFD") 
-     CCIPReceiver(_router)
-     Ownable(msg.sender) 
+     constructor(address _router, address _link)
+     ERC20("CIFD Shares", "CIFD") // 为 ERC20 提供名称和符号
+     CCIPReceiver(_router) // 为 CCIPReceiver 提供路由器地址
+     Ownable(msg.sender) // 为 Ownable 设置所有者
      {
-        
-        require(_foundersWallet != address(0), "Founder's wallet cannot be the zero address.");
-        require(_investorsWallet != address(0), "Investor's wallet cannot be the zero address.");
-        require(_ecosystemWallet != address(0), "Ecosystem wallet cannot be the zero address.");
-
-        foundersWallet = _foundersWallet;
-        investorsWallet = _investorsWallet;
-        ecosystemWallet = _ecosystemWallet;
-
-        unlockTime1Year = block.timestamp + 365 days;
-        unlockTime2Years = block.timestamp + 2 * 365 days;
-        unlockTime3Years = block.timestamp + 3 * 365 days;
-        unlockTime4Years = block.timestamp + 4 * 365 days;
-
-        maxSupply = 500000000 * (10**uint256(decimals()));
-
-        foundersTokens = maxSupply * 20 / 100;//20% founder
-        uint256 investorsTokens = maxSupply * 10 / 100;//10% investor
-        uint256 ecosystemTokens = maxSupply * 70 / 100;//70% ecosystem
-
-        initFounder = foundersTokens /100; // 1% immediately released
-
-        _mint(foundersWallet, initFounder);
-        _mint(investorsWallet, investorsTokens);
-        _mint(ecosystemWallet, ecosystemTokens);
-
+         // 设置CCIP路由器地址和LINK代币地址
          router = IRouterClient(_router);
          linkTokenAddress = _link;
     }
 
-    function unlockFoundersTokens() public onlyOwner {
-        require(block.timestamp >= unlockTime1Year, "Time lock period has not started yet.");
-        require(maxSupply > ERC20.totalSupply(),"Total supply is max.");
-        uint256 currentTimestamp = block.timestamp;
-        uint256 amountToUnlock;
-
-        if (currentTimestamp >= unlockTime4Years) {
-            amountToUnlock = initFounder * 40;
-        } else if (currentTimestamp >= unlockTime3Years) {
-            amountToUnlock = initFounder * 30;
-        } else if (currentTimestamp >= unlockTime2Years) {
-            amountToUnlock = initFounder * 20;
-        } else if (currentTimestamp >= unlockTime1Year) {
-            amountToUnlock = initFounder * 9;
-        } else {
-            return; 
-        }
-
-        _mint(foundersWallet, amountToUnlock);
-        emit TokensUnlocked(foundersWallet, amountToUnlock);
-    }
-
+    // 发送跨链消息和代币
     function sendToChain(
         uint64 destinationChainId,
         address receiver,
@@ -216,22 +160,28 @@ contract CIFDToken is ERC20,CCIPReceiver,Ownable{
             any2EvmMessage.destTokenAmounts[0].token,
             any2EvmMessage.destTokenAmounts[0].amount
         );
+        // 业务逻辑：为指定账户转账对应的代币
         address recipient = abi.decode(any2EvmMessage.data, (address));
         _transfer(s_lastReceivedTokenAddress, recipient, s_lastReceivedTokenAmount);
     }
 
+    // 辅助函数，用于转账代币
     function _transfer(
     address tokenAddress,
     address payable recipient,
     uint256 amount
     ) internal {
+    // 检查代币是否为此合约本身（ERC20代币）
     if (tokenAddress == address(this)) {
         _transferERC20(tokenAddress, recipient, amount);
     } else {
+        // 对于非ERC20代币，您需要实现相应的转账逻辑
+        // 例如，如果代币遵循ERC20标准
         IERC20(tokenAddress).transfer(recipient, amount);
     }
     }
 
+    // 转账ERC20代币的辅助函数
     function _transferERC20(
     address tokenAddress,
     address payable recipient,
@@ -251,6 +201,7 @@ contract CIFDToken is ERC20,CCIPReceiver,Ownable{
     _;
     }
 
+    // 添加到白名单的函数
     function allowlistSource(uint64 sourceChainSelector, address sender) public onlyOwner {
     allowlistedSources[sourceChainSelector][sender] = true;
     }
